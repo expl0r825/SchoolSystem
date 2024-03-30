1387879c1a0aa821bc68dec7f48a11a25a1523a7
expl0r825 <martom825@gmail.com>
2024-03-31 01:53:56 +0200

    asdasda

--- SchoolSystem/Controllers/HomeController.cs
+++ SchoolSystem/Controllers/HomeController.cs
@@ -7,8 +7,17 @@ namespace SchoolSystem.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;            
+        }
+
         public IActionResult Index()
         {
+
+
             return View();
         }
 
